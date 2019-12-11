b58a0719ac91b78d0d9ed18da0298bf389e5fd3a
pmutua <pmutua@live.com>
2019-12-12 02:25:22 +0300

    first commit

---
+++ news/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http  import HttpResponse, Http404
+import datetime as dt
+from django.shortcuts import render,redirect
+
+from .models import Article
+
+# Create your views here.
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+#..........
+def news_of_day(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+
+def past_days_news(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        day = convert_dates(date) 
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html', {"date": date,"news":news})
+
+
+def search_results(request):
+    
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
